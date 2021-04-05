7d778e425f6a9e5b1addc84b45f4408cfb0b121c
Lokesh Rajgadkar <lokeshrajgadkar13@gmail.com>
2021-04-05 11:58:18 +0530

    weekday & weekend pages added

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.send("Hello");
+    var today = new Date();
+    var currentDay = today.getDay;
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.sendFile(__dirname + "/weekend.html");
+    } else{
+        res.sendFile(__dirname + "/weekday.html");
+
+    }
 });
 
 
