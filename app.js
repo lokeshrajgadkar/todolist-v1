c2a574e6751c4763188776737a090bda915d1de6
Lokesh Rajgadkar <lokeshrajgadkar13@gmail.com>
2021-04-06 23:10:24 +0530

    Functionality added & css style added

--- app.js
+++ app.js
@@ -3,21 +3,37 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-    var today = new Date();
-    var currentDay = today.getDay;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + "/weekend.html");
-    } else{
-        res.sendFile(__dirname + "/weekday.html");
+app.get("/", function (req, res) {
+    
+    let today = new Date(); 
+        
+    let options = {
+        day: "numeric",
+        weekday: "long",
+        month: "long",
+    };
 
-    }
+    let day = today.toLocaleDateString("hi-IN", options);
+    
+    res.render("list", { kindOfDay: day, newListItems: items});
+
 });
 
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+    console.log(items);
+    res.redirect("/");
+})
 
-app.listen(3000, function(){
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
